389e27504f6b375548e2a8432da2caaae7acd8c4
ldd <adjkldd@126.com>
2016-05-28 19:31:15 +0800

    feature: finish minimax algorithm

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-$.prototype.center = function (ratio) {
+$.prototype.square = function (ratio) {
     var parent = $(this).parent();
     var W = 0,
         H = 0;
@@ -14,6 +14,7 @@ $.prototype.center = function (ratio) {
 
     var length = (W <= H ? W : H) * ratio;
 
+
     parent.css({
         "padding": (H - length) / 2 + "px " + (W - length) / 2 + "px"
     });
@@ -24,66 +25,232 @@ $.prototype.center = function (ratio) {
     });
 };
 
+$.prototype.rectangular = function (ratio) {
+    var parent = $(this).parent();
+    var W = 0,
+        H = 0;
+
+    if (parent.is("body")) {
+        $("body").css("margin", 0);
+        W = $(window).width();
+        H = $(window).height();
+    } else {
+        W = parent.width();
+        H = parent.height();
+    }
+
+    var w = W * ratio,
+        h = H * ratio;
+
+    parent.css({
+        "padding": (H - h) / 2 + "px " + (W - w) / 2 + "px"
+    });
+
+    $(this).css({
+        "width": w,
+        "height": h
+    });
+};
+
 function popup(parent, child) {
 
     parent.css("position", "relative");
 
-    var overlay = $('<div class="overlay"></div>').css({
-        "position": "absolute",
-        "top": 0,
-        "left": 0,
-        "width": parent.width(),
-        "height": parent.height(),
-        "background-color": "rgba(255, 255, 255, 0.5)",
-        "z-index": 100,
-        "display": "none"
-    });
+    var overlay = $(".overlay");
+    if (parent.children(".overlay").length === 0) {
 
-    overlay.appendTo(parent).fadeIn('slow');
-    child.appendTo(overlay);
+        overlay = $('<div class="overlay"></div>').css({
+            "position": "absolute",
+            "top": 0,
+            "left": 0,
+            "width": parent.width(),
+            "height": parent.height(),
+            "background-color": "rgba(255, 255, 255, 0.5)",
+            "z-index": 100,
+            "display": "none"
+        });
+        overlay.appendTo(parent);
+    }
+    overlay.html('');
+
+
+    overlay.fadeIn('slow');
+    child.appendTo(overlay).show();
 
 
     return overlay;
 }
 
 
+var $X = '<i class="fa fa-close"></i>',
+    $O = '<i class="fa fa-circle-o"></i>',
+    $cursor = $(".cursor");
+
 var ticTacToe = {
-    xo: ['<i class="fa fa-close"></i>', '<i class="fa fa-circle-o"></i>'],
-    index: 0,
-    turn: function () {
-        return this.xo[(this.index++) % 2];
+    grid: [[null, null, null], [null, null, null], [null, null, null]],
+    human: $O,
+    computer: $X,
+    win: false,
+    full: function () {
+        for (var i = 0; i < 3; ++i) {
+            for (var j = 0; j < 3; ++j) {
+                if (this.grid[i][j] === null)
+                    return false;
+            }
+        }
+        return true;
+    },
+    checkWin: function (a) {
+        var grid = this.grid;
+        this.win = (grid[0][0] === a && grid[0][1] === a && grid[0][2] === a) ||
+            (grid[1][0] === a && grid[1][1] === a && grid[1][2] === a) ||
+            (grid[2][0] === a && grid[2][1] === a && grid[2][2] === a) ||
+            (grid[0][0] === a && grid[1][1] === a && grid[2][2] === a) ||
+            (grid[0][2] === a && grid[1][1] === a && grid[2][0] === a) ||
+            (grid[0][0] === a && grid[1][0] === a && grid[2][0] === a) ||
+            (grid[0][1] === a && grid[1][1] === a && grid[2][1] === a) ||
+            (grid[0][2] === a && grid[1][2] === a && grid[2][2] === a);
+        return this.win;
+    },
+    render: function () {
+        for (var r = 0; r < 3; ++r) {
+            for (var c = 0; c < 3; ++c) {
+                if (this.grid[r][c] !== null) {
+                    var theCell = $('.cell[data-pos="' + r + ',' + c + '"]');
+                    if (theCell.html().toString() === "")
+                        theCell.html(this.grid[r][c]);
+                }
+            }
+        }
+    },
+    clearBoard: function () {
+        this.grid = [[null, null, null], [null, null, null], [null, null, null]];
+        for (var r = 0; r < 3; ++r) {
+            for (var c = 0; c < 3; ++c) {
+
+                var theCell = $('.cell[data-pos="' + r + ',' + c + '"]');
+
+                theCell.html("");
+
+            }
+        }
+    },
+    minimax: function (maximize, depth) {
+        depth = depth || 0;
+        var vector = [];
+        for (var i = 0; i < 3; ++i) {
+            for (var j = 0; j < 3; ++j) {
+                if (this.grid[i][j] === null) {
+                    this.grid[i][j] = maximize ? this.computer : this.human;
+                    // for debug:
+                    // document.querySelector('.cell[data-pos="' + i + ',' + j + '"]').innerText = this.grid[i][j];
+                    if (this.checkWin(this.grid[i][j])) {
+                        vector.push([i, j, maximize ? (10 - depth) : -(10 - depth)]);
+                    } else {
+                        var ret = this.minimax(!maximize, depth + 1);
+                        if (ret === undefined) {
+                            vector.push([i, j, 0]);
+                        } else {
+                            vector.push([i, j, ret[2]]);
+                        }
+                    }
+                    this.grid[i][j] = null;
+                    // for debug:
+                    // document.querySelector('.cell[data-pos="' + i + ',' + j + '"]').innerText = "";
+                }
+            }
+        }
+        
+        return vector.sort(function (a, b) {
+            return maximize ? (b[2] - a[2]) : (a[2] - b[2]);
+        })[0]; // maximum score
+    },
+    computerTurn: function () {
+        var pos = this.minimax(true),
+            x = pos[0],
+            y = pos[1];
+        ticTacToe.grid[x][y] = ticTacToe.computer;
+        ticTacToe.render();
+
+        if (ticTacToe.checkWin($('.cell[data-pos="' + x + ',' + y + '"]').html().toString())) {
+            $(".game-over .result").html(ticTacToe.grid[x][y] + " wins!");
+            popup($(".chessboard"), $(".game-over"));
+        } else if (ticTacToe.full()) {
+            $(".game-over .result").html("It's a draw!");
+            popup($(".chessboard"), $(".game-over"));
+        }
+    },
+    availableCell: function () {
+        var available = [];
+        for (var i = 0; i < 3; ++i) {
+            for (var j = 0; j < 3; ++j) {
+                if (this.grid[i][j] === null) {
+                    available.push([i, j]);
+                }
+            }
+        }
+        return available;
     }
 };
 
+
 $(".cell").click(function () {
-    $(this).html(ticTacToe.turn());
+    // alert($(this).attr("data-pos"));
+    var pos = $(this).attr("data-pos").split(','),
+        x = pos[0],
+        y = pos[1];
+    ticTacToe.grid[x][y] = ticTacToe.human;
+    ticTacToe.render();
+    if (ticTacToe.checkWin($(this).html().toString())) {
+        $(".game-over .result").html(ticTacToe.grid[x][y] + " wins!");
+        popup($(".chessboard"), $(".game-over"));
+    } else if (ticTacToe.full()) {
+        $(".game-over .result").html("It's a draw!");
+        popup($(".chessboard"), $(".game-over"));
+    } else {
+        ticTacToe.computerTurn();
+    }
+
+    $(".restart").click(function () {
+        ticTacToe.clearBoard();
+        ticTacToe.render();
+        $(".overlay").css("display", "none");
+    });
+
+
 });
 
 
 $(function () {
 
     $(window).resize();
-    var overlay = popup($(".chessboard"), $(".settings").show());
+    var overlay = popup($(".chessboard"), $(".settings"));
 
     $("#x").click(function () {
         overlay.fadeOut('slow');
+        ticTacToe.human = $X;
+        ticTacToe.computer = $O;
     });
 
     $("#o").click(function () {
         overlay.fadeOut('slow');
+        ticTacToe.human = $O;
+        ticTacToe.computer = $X;
+        ticTacToe.computerTurn();
     });
+
 
 });
 
 $(window).resize(function () {
-    var app = $(".chessboard").css("display", "block");
-    var cell = $(".cell");
+    var board = $(".chessboard").css("display", "block"),
+        cell = $(".cell");
 
 
     if ($(window).width() < 400 || $(window).height() < 400) {
-        app.center(0.8);
+        board.square(0.8);
     } else {
-        app.center(0.6);
+        board.square(0.6);
     }
 
 
@@ -92,5 +259,5 @@ $(window).resize(function () {
         "font-size": cell.height() * 0.7
     });
 
-    $(".overlay").width(app.width()).height(app.height());
+    $(".overlay").width(board.width()).height(board.height());
 });
